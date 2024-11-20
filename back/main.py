c496344eba007717325d5bdea0c84cd5b8c34c3a
RiedelNicolas <nariedel99@gmail.com>
2024-11-19 22:55:14 -0300

    remove cors validation

--- back/main.py
+++ back/main.py
@@ -8,25 +8,20 @@ from dotenv import load_dotenv
 
 app = FastAPI()
 
-load_dotenv()
-DATABASE_URL = getenv("DATABASE_URL")
-FRONTEND_URL = getenv("FRONTEND_URL")
-
-if not DATABASE_URL:
-    raise ValueError("DATABASE_URL environment variable is not set")
-if not FRONTEND_URL:
-    raise ValueError("FRONTEND_URL environment variable is not set")
-
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[FRONTEND_URL],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
+load_dotenv()
+DATABASE_URL = getenv("DATABASE_URL")
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL environment variable is not set")
 engine = create_engine(DATABASE_URL)
+
 
 def get_session():
     with Session(engine) as session:
