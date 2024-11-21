235b03b598672b22f7d5940814333ea95a4a5db6
RiedelNicolas <nariedel99@gmail.com>
2024-11-21 09:32:38 -0300

    change vite config

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/intro-ejemplo-despliegue/'
+  base: '/repo-ejemplo-clase-123/'
 })
